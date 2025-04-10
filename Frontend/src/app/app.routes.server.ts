3f4ac925155ace6dc41ab612e8d40e6119a2025d
Sinha-Anmol-Mechlin <Anmol.sinha@mechlintech.com>
2025-04-11 01:27:40 +0530

    Angular.json changes

--- Frontend/src/app/app.routes.server.ts
+++ Frontend/src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'content/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => {
+      // Return a Promise of parameters to prerender
+      return Promise.resolve([{ id: '1' }, { id: '2' }, { id: '3' }]);
+    }
+  },
+  {
+    path: 'student-dashboard/content-view/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: () => {
+      // Return a Promise of parameters to prerender
+      return Promise.resolve([{ id: '1' }, { id: '2' }, { id: '3' }]);
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
