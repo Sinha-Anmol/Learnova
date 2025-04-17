244718eb7830b8a83d1dc5a9f4513185ba9c194e
Sinha-Anmol-Mechlin <Anmol.sinha@mechlintech.com>
2025-04-18 04:28:11 +0530

    UpdateOnDatabaseChange

---
+++ Backend/EduPortalAPI/EduPortalAPI/Migrations/20250417225653_PhoneNumber3.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduPortalAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhoneNumber3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "phno",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "phno",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
